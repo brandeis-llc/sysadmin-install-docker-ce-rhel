778767a2a34c492bf145d9b29d4ade791359aeec
Keigh Rim <keigh.rim@gmail.com>
2017-09-11 15:08:24 -0400

    added docker-ce installation script

---
+++ install-docker-ce.sh
@@ -0,0 +1,27 @@
+# remove any existing installation
+sudo yum remove docker docker-common docker-selinux docker-engine
+
+# add yum repository 
+# note that starting from docker-ce 17.06, docker team decided to keep RHEL users from installing community edition, 
+# so that they and redhat can make money from them.
+# this repository only have docker-ce up to 17.05
+sudo cp ./docker.repo /etc/yum.repos.d/
+
+# update repo cache and install docker
+sudo yum makecache fat && sudo yum install docker-engine docker-engine-selinux
+
+# start docker as a system service
+sudo systemctl start docker
+
+# add users to docker group so they can use docker commands without sudoing
+# current user would always be added, and additional user can be passed as arguments
+while [ $# -gt 0 ]; do 
+    sudo usermode -aG docker $USER
+    if [ $1 != $USER ] ; then 
+        sudo usermod -aG docker $1
+    fi
+    shift
+done
+
+# test everything 
+docker run hello-world
